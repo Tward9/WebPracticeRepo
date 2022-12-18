239422c8ad96c43306278f9a7fb83c7208e0acd5
Theo Ward <theoward9@gmail.com>
2022-12-18 16:55:02 -0600

    reactangle and square class

---
+++ HackerRankProblems/rectangleClass.js
@@ -0,0 +1,33 @@
+// Task
+
+// We provide the implementation for a Rectangle class in the editor. Perform the following tasks:
+
+// Add an area method to Rectangle's prototype.
+// Create a Square class that satisfies the following:
+// It is a subclass of Rectangle.
+// It contains a constructor and no other methods.
+// It can use the Rectangle class' area method to print the area of a Square object.
+// Locked code in the editor tests the class and method implementations and prints the area values to STDOUT.
+
+//solution
+class Rectangle {
+    constructor(w, h) {
+        this.w = w;
+        this.h = h;
+    }
+}
+Rectangle.prototype.area = function () {
+    let area = this.w * this.h;
+    return area;
+}
+class Square extends Rectangle {
+    constructor(w) {
+        super(w);
+        this.h = w;
+    }
+
+}
+
+const rec = new Rectangle(3, 4);
+const sqr = new Square(3);
+console.log(sqr.area());
