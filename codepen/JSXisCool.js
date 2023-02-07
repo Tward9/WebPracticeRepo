c2bfbf2b92a879659c7bb4da062a982204c01490
Theo Ward <theoward9@gmail.com>
2023-02-06 18:12:10 -0600

    react challenges found in google

---
+++ codepen/JSXisCool.js
@@ -0,0 +1,13 @@
+/** 
+  Challenge: Display `JSX is cool!` as an output
+  
+  Solution: https://codepen.io/angelo_jin/pen/xxXrZLd
+  Video for reference: https://youtu.be/VzNNjNmbXpY
+**/
+function App() {
+  return(
+    <h1>JSX is cool!</h1>
+    )
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
